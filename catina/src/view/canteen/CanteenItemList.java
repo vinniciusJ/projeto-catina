6f458bc801efd06f4a0ba5c8469077e94f1a80b9
Vinicius <vinnicius.jimenez@gmail.com>
2021-07-09 12:02:57 -0300

    finished the base of CanteenItemList

--- catina/src/view/canteen/CanteenItemList.java
+++ catina/src/view/canteen/CanteenItemList.java
@@ -5,12 +5,14 @@
  */
 package view.canteen;
 
-import java.awt.Color;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.util.ArrayList;
 import javax.swing.BoxLayout;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
-import models.Item;
+import models.CanteenItem;
 import view.View;
 
 
@@ -19,45 +21,83 @@ import view.View;
  * @author Vinicius Jimenez
  */
 public class CanteenItemList extends JPanel implements View{
-   private ArrayList<Item> items;
+   private ArrayList<CanteenItem> items;
    
-    private class ListItem extends JPanel{
+    private final class ListItem extends JPanel implements View{
+        private final CanteenItem data;
         
-        public ListItem(Item item){
+        public ListItem(CanteenItem item){
+            this.data = item;
             
+            this.init();
+            this.paint();
+        }
+
+        @Override
+        public void init() {
+            this.setBorder(new EmptyBorder(0, 0, 20, 0));
+            this.setLayout(new GridBagLayout());
+        }
+
+        @Override
+        public void paint() {
+            var itemName = new JLabel(this.data.getItem().getName());
+            var itemPrice = new JLabel("R$ " + Float.toString(this.data.getItem().getPrice()));
+            var itemQtty = new JLabel(this.data.getQuantity() + " unidades");
+            
+            var itemNameGc = new GridBagConstraints();
+            var itemPriceGc = new GridBagConstraints();
+            var itemQttyGc = new GridBagConstraints();
+            
+            itemNameGc.fill = GridBagConstraints.HORIZONTAL;
+            itemNameGc.gridx = 0;
+            itemNameGc.gridy = 0;
+            itemNameGc.gridwidth = 2;
+            
+            itemPriceGc.fill = GridBagConstraints.HORIZONTAL;
+            itemPriceGc.gridx = 2;
+            itemPriceGc.gridy = 0;
+            
+            itemQttyGc.fill = GridBagConstraints.HORIZONTAL;
+            itemQttyGc.gridx = 3;
+            itemQttyGc.gridy = 0;
+            
+            
+            this.add(itemName, itemNameGc);
+            this.add(itemPrice, itemPriceGc);
+            this.add(itemQtty, itemQttyGc);
         }
     }
     
     public CanteenItemList(){}
     
-    public CanteenItemList(ArrayList<Item> items){
+    public CanteenItemList(ArrayList<CanteenItem> items){
         this.items = items;
     }
     
     
     @Override
     public void init() {
-        this.setBackground(Color.red);
         this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
         this.setBorder(new EmptyBorder(0, 40, 0, 40));
     }
 
     @Override
     public void paint() {
-        
+        this.items.forEach(item -> this.add(new ListItem(item)));
     }
 
     /**
      * @param items the items to set
      */
-    public void setItems(ArrayList<Item> items) {
+    public void setItems(ArrayList<CanteenItem> items) {
         this.items = items;
     }
 
     /**
      * @return the items
      */
-    public ArrayList<Item> getItems() {
+    public ArrayList<CanteenItem> getItems() {
         return items;
     }
 }
